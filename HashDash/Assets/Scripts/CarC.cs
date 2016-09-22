1be2ea1a16c50cc1b1fba0b5fea754c85ba851f1
Adrian Flaherty <therealao@gmail.com>
2016-09-22 21:37:50 +0100

    Update1
    Update to Car Functionality, animated wheels added

---
+++ HashDash/Assets/Scripts/CarC.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+[System.Serializable]
+public class CarC : MonoBehaviour 
+{
+	public WheelCollider leftWheel;
+	public WheelCollider rightWheel;
+	public WheelCollider backLeftWheel;
+	public WheelCollider backRightWheel;
+	public bool motor; // is this wheel attached to motor?
+	public bool steering; // does this wheel apply steer angle?
+}
+
+
+public class SimpleCarController : MonoBehaviour 
+{
+	public List<CarC> axleInfos; // the information about each individual axle
+		public float maxMotorTorque = 600.0f; // maximum torque the motor can apply to wheel
+		public float maxSteeringAngle = 30.0f; // maximum steer angle the wheel can have
+	public void FixedUpdate()
+	{
+		float motor = maxMotorTorque * Input.GetAxis("Vertical");
+		float steering = maxSteeringAngle * Input.GetAxis("Horizontal");
+		foreach (CarC axleInfo in axleInfos) {
+			if (axleInfo.steering) {
+				axleInfo.leftWheel.steerAngle = steering;
+				axleInfo.rightWheel.steerAngle = steering;
+				axleInfo.backLeftWheel.steerAngle = steering;
+				axleInfo.backRightWheel.steerAngle = steering;
+			}
+			if (axleInfo.motor) {
+				axleInfo.leftWheel.motorTorque = motor;
+				axleInfo.rightWheel.motorTorque = motor;
+				axleInfo.backLeftWheel.motorTorque = motor;
+				axleInfo.backRightWheel.motorTorque = motor;
+			}
+		}
+	}
+}
+/*
+public class GetCollider(n : int)
+{
+	return wheels[n].gameObject.GetComponent(WheelCollider);
+}
+*/
--- HashDash/Assets/Scripts/CarControls.js
+++ HashDash/Assets/Scripts/CarControls.js
@@ -8,27 +8,35 @@ var power=0.0;
 var brake=0.0;
 var steer=0.0;
 var body:Rigidbody;
-var maxSteer=25.0;
- 
+var maxSteer=35.0;
 function Start()
 {
     this.GetComponent.<Rigidbody>().centerOfMass = Vector3(0,-0.5,0.3);
     //body.centreOfMass = Vector3(0,-0.5,0.3);
 }
  
-function Update () {
+function Update () 
+{
     power=Input.GetAxis("Vertical") * enginePower * Time.deltaTime * 250.0;
     steer=Input.GetAxis("Horizontal") * maxSteer;
-    brake=Input.GetKey("space") ? body.mass * 0.3: 0.0;
+    brake=Input.GetKey("space") ? body.mass * 0.5: 0.0;
    
     GetCollider(0).steerAngle=steer;
     GetCollider(1).steerAngle=steer;
+    ApplyLocalPositionToVisuals(GetCollider(0));
+    ApplyLocalPositionToVisuals(GetCollider(2));
+    ApplyLocalPositionToVisuals(GetCollider(1));
+    ApplyLocalPositionToVisuals(GetCollider(3));
+
+
    
     if(brake > 0.0){
         GetCollider(0).brakeTorque=brake;
         GetCollider(1).brakeTorque=brake;
         GetCollider(2).brakeTorque=brake;
         GetCollider(3).brakeTorque=brake;
+        GetCollider(0).motorTorque=0.0;
+        GetCollider(1).motorTorque=0.0;
         GetCollider(2).motorTorque=0.0;
         GetCollider(3).motorTorque=0.0;
     } else {
@@ -41,7 +49,27 @@ function Update () {
     }
 }
  
-function GetCollider(n : int) : WheelCollider{
-    return wheels[n].gameObject.GetComponent(WheelCollider);
-}
+function GetCollider(n : int) : WheelCollider
+	{
+	    return wheels[n].gameObject.GetComponent(WheelCollider);
+	    
+	    
+	}
+
+function ApplyLocalPositionToVisuals(coll:WheelCollider)
+	{
+		 if (coll.transform.childCount == 0) 
+		 {
+		 	return;
+	 	 }
+		 var visualWheel:Transform = coll.transform.GetChild(0);
+		 var position : Vector3;
+		 var quat: Quaternion;
+		 //(out position:Vector3, out quat:Quaternion);
+		 coll.GetWorldPose(position, quat);
+         visualWheel.transform.position = position;
+         visualWheel.transform.rotation = quat;
+         
+		 
+ }
  
